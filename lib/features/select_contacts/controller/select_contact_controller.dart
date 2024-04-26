9bcc39d60163305dcdd788fb2652dc0000b1063c
SumanRasaili <rasailisuman123@gmail.com>
2024-04-26 14:30:22 +0545

    get the contacts from device contacts and shown

--- lib/features/select_contacts/controller/select_contact_controller.dart
+++ lib/features/select_contacts/controller/select_contact_controller.dart
@@ -2,8 +2,8 @@ import 'package:flutter_contacts/contact.dart';
 import 'package:guffgaff/features/select_contacts/repository/select_contact_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final selectContactControllerProvider =
-    FutureProvider<List<Contact>>((ref) async {
+final getContactsProvider =
+    FutureProvider((ref) async {
   final selectCountryRepo = ref.watch(selectContactRepositoryProvider);
 
   return selectCountryRepo.getContacts();
--- lib/features/select_contacts/screens/select_contacts_screen.dart
+++ lib/features/select_contacts/screens/select_contacts_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:guffgaff/common/widgets/error_screen.dart';
+import 'package:guffgaff/common/widgets/loader.dart';
+import 'package:guffgaff/features/select_contacts/controller/select_contact_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SelectContactsScreen extends ConsumerWidget {
@@ -18,7 +21,42 @@ class SelectContactsScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Select Contact"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
       ),
+      body: ref.watch(getContactsProvider).when(data: (contactList) {
+        return RefreshIndicator(
+          onRefresh: () {
+            return ref.read(getContactsProvider.future);
+          },
+          child: ListView.builder(
+              itemCount: contactList.length,
+              itemBuilder: (context, index) {
+                final contact = contactList[index];
+             
+                return ListTile(
+                  leading: contact.photo == null
+                      ? null
+                      : CircleAvatar(
+                          radius: 30,
+                          backgroundImage: MemoryImage(contact.photo!)),
+                  title: Text(contact.displayName),
+                );
+              }),
+        );
+      }, error: (error, stackTrace) {
+        return ErrorScreen(error: error.toString());
+      }, loading: () {
+        return const Loader();
+      }),
     );
   }
 }
