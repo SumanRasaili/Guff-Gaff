5e9234d7e95b2d1a696b2fb84d5fa718cb63448c
SumanRasaili <rasailisuman123@gmail.com>
2024-12-12 22:51:13 +0545

    minor fixes

--- lib/features/landing/landing_screen.dart
+++ lib/features/landing/landing_screen.dart
@@ -20,47 +20,49 @@ class LandingScreen extends ConsumerWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
         body: SafeArea(
-            child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(
-          height: 50,
-        ),
-        const Text(
-          "Welcome to Guff Gaff",
-          style: TextStyle(fontSize: 33, fontWeight: FontWeight.w600),
-        ),
-        SizedBox(
-          height: size.height / 9,
-        ),
-        Image.asset(
-          'assets/images/bg.png',
-          height: 340,
-          width: 340,
-          color: tabColor,
-        ),
-        SizedBox(
-          height: size.height / 9,
-        ),
-        const Padding(
-          padding: EdgeInsets.all(15.0),
-          child: Text(
-            'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: greyColor,
+            child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            "Welcome to Guff Gaff",
+            style: TextStyle(fontSize: 33, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: size.height / 9,
+          ),
+          Image.asset(
+            'assets/images/bg.png',
+            height: 340,
+            width: 340,
+            color: tabColor,
+          ),
+          SizedBox(
+            height: size.height / 9,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: greyColor,
+              ),
             ),
           ),
-        ),
-        SizedBox(
-            width: size.width * 0.80,
-            child: CustomButton(
-              text: "AGREE & CONTINUE",
-              onPressed: () {
-                context.push(LoginScreen.routeName);
-              },
-            ))
-      ],
+          SizedBox(
+              width: size.width * 0.80,
+              child: CustomButton(
+                text: "AGREE & CONTINUE",
+                onPressed: () {
+                  context.push(LoginScreen.routeName);
+                },
+              ))
+        ],
+      ),
     )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:guffgaff/firebase_options.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'config/config.dart';
 
 void main() async {
